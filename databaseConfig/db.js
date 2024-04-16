2c396f2ffa8698a190684221c3d561dba3d4e181
Amit Kumar <115470270+amitsdlv1234@users.noreply.github.com>
2024-04-16 13:44:27 +0530

    new

--- databaseConfig/db.js
+++ databaseConfig/db.js
@@ -1,6 +1,5 @@
 import Sequelize from "sequelize";
 import dotenv from "dotenv";
-import mysql2 from "mysql2";
 dotenv.config();
 const username = process.env.DB_USERNAME;
 const database = process.env.DB_DATABASENAME;
@@ -9,7 +8,7 @@ const host = process.env.DB_HOST;
 
 const sequelize = new Sequelize(database, username, password, {
   host: host,
-  dialect: mysql2
+  dialect: 'mysql2',
 });
 
 export default sequelize;
